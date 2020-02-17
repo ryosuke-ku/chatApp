bfa06a173b10f8bcffb9e68f852547233a54131b
RYOSUKE <naist1020@gmail.com>
2020-02-17 15:50:54 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -18,22 +18,69 @@
   });
   const collection = db.collection('messages');
 
+  const auth = firebase.auth();
+
   const message = document.getElementById('message');
   const form = document.querySelector('form');
+  const messages = document.getElementById('messages');
+  const login = document.getElementById('login');
+  const logout = document.getElementById('logout');
 
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    collection.add({
-        message: message.value
-    })
-    .then(doc => {
-        console.log(`${doc.id} added!`);
-        message.value = '';
+  login.addEventListener('click', () => { 
+    auth.signInAnonymously();
+  });
+ 
+  logout.addEventListener('click', () => { 
+    auth.signOut();
+  });
+
+  auth.onAuthStateChanged(user => {
+    if (user){
+        collection.orderBy('created').onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if (change.type === 'added'){
+                  const li =document.createElement('li');
+                  li.textContent = change.doc.data().message;
+                  messages.appendChild(li);
+                }
+            });
+        });
+        console.log(`Logged in as: ${user.uid}`);
+        login.classList.add('hidden');
+        [logout, form, messages].forEach(el => {
+            el.classList.remove('hidden');
+        });
         message.focus();
-    })
-    .catch(error => {
-        console.log(error);
+        return;
+    }
+    console.log('Nobody is logged in');
+    login.classList.remove('hidden');
+    [logout, form, messages].forEach(el => {
+        el.classList.add('hidden');
     });
   });
 
-  message.focus();
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const val = message.value.trim();
+        if (val === ""){
+            return;
+        }
+
+        message.value = '';
+        message.focus();
+
+        collection.add({
+            message: val,
+            created: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .then(doc => {
+            console.log(`${doc.id} added!`);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+  });
+
